6c650c68be61779ac6dc17350ee9186c9e00ea55
Jorge Soto <djsmoothzouk@gmail.com>
2021-01-16 22:00:14 -0600

    api fetch and console logged data complete

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Route path="/hero-search">
         <HeroSearch />
       </Route>
-      <Route path="/">
+      <Route exact path="/">
         <SuperheroData />
       </Route>
     </div>
--- src/SuperheroData/HeroSearch.js
+++ src/SuperheroData/HeroSearch.js
@@ -4,7 +4,18 @@ import React from 'react'
 const HeroSearch = (props) => {
 
     return (
+        <div>
         <h1>This is the HeroSearch section</h1>
+            <form>
+                <input
+                   type="text"
+                   name="searchHero"
+                   placeholder="Enter name"></input>
+                <input
+                type="submit"
+                value="submit"></input>
+            </form>
+        </div>
     )
 }
 

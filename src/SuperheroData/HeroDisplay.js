ff63428c9f225e0925e6e5ac3e8006af157cc2c5
Jorge Soto <djsmoothzouk@gmail.com>
2021-01-20 13:16:22 -0600

    mapping done and items are displaying

--- src/SuperheroData/HeroDisplay.js
+++ src/SuperheroData/HeroDisplay.js
@@ -7,27 +7,44 @@ const HeroDisplay = (props) => {
 
     const apiURL = `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/10218084998348396/search/${searchname}/`
 
-    const [hero, setHero] = React.useState("")
+    const [hero, setHero] = React.useState(null)
+    
 
     const getHero = async () => {
         const response = await fetch(apiURL,{headers:{'Origin': 'https://superheroapi.com'}})
         const data = await response.json()
-        setHero(data[''])
-        console.log('this has items from name section', data)
-        console.log('this is details', data)
+        setHero(data)
+        // console.log('this has items from name section', data)
+        console.log('this is details', data.results[0])
     }
     React.useEffect(() => {
         getHero()
       }, [])
 
 
-    
-    return (
-        <>
-        <h1>This is the HeroDisplay section</h1>
-         
-        </>
-    )
+      
+      const loaded = () => {
+          
+       return  hero.results.map(item => {
+            
+              return (
+                  <div className="hero-card">
+                  <h1>This is the HeroDisplay section</h1>
+                  <img src={item.image.url}></img>
+                  <h2>Name: {item.name}</h2>
+                  <h2>Race: {item.appearance.race}</h2>
+                  <h2>Height: {item.appearance.height[0]}</h2>
+                </div>
+
+              )
+            })
 }
+
+      const loading = () => {
+         return <h2>No Superhero to display</h2>
+}
+         return hero ? loaded() : loading()
+}
+
 
 export default HeroDisplay
--- src/SuperheroData/index.js
+++ src/SuperheroData/index.js
@@ -1,35 +1,34 @@
 import React from 'react'
-import HeroSearch from './HeroSearch'
-import HeroDisplay from './HeroDisplay'
+// import HeroSearch from './HeroSearch'
+// import HeroDisplay from './HeroDisplay'
 
 
 
 
 const SuperheroData = (props) => {
 
-    const searchname = props?.heroData?.searchname
+    // const searchname = props?.heroData?.searchname
 
-    const apiURL = `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/10218084998348396/search/name=${searchname}/`
+    // const apiURL = `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/10218084998348396/search/name=${searchname}/`
 
-    const [hero, setHero] = React.useState("")
+    // const [hero, setHero] = React.useState("")
 
-    const getHero = async () => {
-        const response = await fetch(apiURL,{headers:{'Origin': 'https://superheroapi.com'}})
-        const data = await response.json()
-        setHero(data[''])
-        // console.log('this has items from name section', data)
-        // console.log('this is details', data)
-    }
-    React.useEffect(() => {
-        getHero()
-      }, [])
+    // const getHero = async () => {
+    //     const response = await fetch(apiURL,{headers:{'Origin': 'https://superheroapi.com'}})
+    //     const data = await response.json()
+    //     setHero(data[''])
+    //     // console.log('this has items from name section', data)
+    //     // console.log('this is details', data)
+    // }
+    // React.useEffect(() => {
+    //     getHero()
+    //   }, [])
 
       
     return (
         <>
         <h1>This is the SuperheroData section</h1>
-       {/* <HeroSearch herosearch={getHero}/>
-       <HeroDisplay hero={hero} /> */}
+
         </>
     )
 }

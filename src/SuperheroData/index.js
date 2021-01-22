05d22cf3e655b3e950b170cea0e9c2ab7a03356d
Jorge Soto <djsmoothzouk@gmail.com>
2021-01-22 13:06:26 -0600

    final push before due

--- src/SuperheroData/index.js
+++ src/SuperheroData/index.js
@@ -30,11 +30,11 @@ const SuperheroData = () => {
         <h1>This is the Superhero Database</h1>
         <section className="home-text">
             <h3> This database has general information regarding most heroes or villians across the comic, animated, and cinematic universe.</h3>
-            <h2>Marvel/DC Comics</h2>
+            <h2 className="example-title">Marvel/DC Comics</h2>
             <img className="image" src="https://res.cloudinary.com/acurunner79/image/upload/v1611289382/top-10-featured_kkzevu.jpg"></img>
-            <h2>Star Wars</h2>
+            <h2 className="example-title">Star Wars</h2>
             <img className="image"src="https://res.cloudinary.com/acurunner79/image/upload/v1611289374/Ra4071b5bd9a4a11ae28d697daa76ad52_ffgeg1.jpg"></img>
-            <h2>Movies</h2>
+            <h2 className="example-title">Movies</h2>
             <img className="image" src="https://res.cloudinary.com/acurunner79/image/upload/v1611290787/sci-fi-movie-monsters_zy3o0y.jpg"></img>
         </section>
         </div>
--- src/components/About.js
+++ src/components/About.js
@@ -10,8 +10,7 @@ const About = () => {
           <h1>Welcome to my Superhero Database. This database has a broad variety of fictional characters that we all love. I hope you enjoy it! </h1>
           {/* <h3>Embed YouTube video - <a href="https://www.cluemediator.com">Clue Mediator</a></h3> */}
           <div className="vid">
-            <ReactPlayer 
-                          url="https://www.youtube.com/watch?v=OO0l9Yuhc0o"/>
+            <ReactPlayer url="https://www.youtube.com/watch?v=OO0l9Yuhc0o"/>
          </div>
         </article>
     )
